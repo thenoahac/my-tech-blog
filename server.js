7f60670a6e9bea600a354949071c1d0d30b148f1
Noah Carralero <nole2kgames@gmail.com>
2022-09-08 22:12:35 -0400

    Created Server, Connection, and Schema Files

---
+++ server.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt');
+const exphbs = require('express-handlebars');
+const express = require('express');
+const mysql = require('mysql2');
+const path = require('path');
+const routes = require('./routes');
+const session = require('express-session');
+
+require('dotenv').config();
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'super secret time',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const hbs = exphbs.create({});
+app.use(express.static('public'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+app.get('/', (req,res) => {
+    res.render('home',
+    );
+    }
+);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on 3001'));
+  });
